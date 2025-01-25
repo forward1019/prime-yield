97467d526f68bfec94097be3879760df72b8a9f2
forward1019 <forwardhj1019@gmail.com>
2025-01-25 16:23:03 +0000

    update landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+if (process.env.NEXT_PUBLIC_TEMPO) {
+  nextConfig["experimental"] = {
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+  }
+}
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
+import { TempoInit } from "./tempo-init";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script src="https://api.tempolabs.ai/proxy-asset?url=https://storage.googleapis.com/tempo-public-assets/error-handling.js" />
+        <TempoInit />
         {children}
       </body>
     </html>
